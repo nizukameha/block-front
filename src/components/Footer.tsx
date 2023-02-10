43a3a329cbbdf19599bfe1e88f2371b922e50e8a
Lexa <axel.reviron@gmail.com>
2023-02-10 16:00:36 +0100

    footer content

---
+++ src/components/Footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+        <>
+            <hr/>
+            <div className="row">
+                <div className="col-12 text-center">
+                    <a href="https://axel-reviron.netlify.app/" className="footerLink">Axel Reviron</a>
+                </div>
+            </div>
+        </>
+    );
+}
