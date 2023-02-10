3e6fbb56c88065a4ba5499549563acc8f4289414
Lexa <axel.reviron@gmail.com>
2023-02-10 15:18:29 +0100

    require bootstrap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import '@/styles/style.css'
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    require("bootstrap/dist/js/bootstrap.bundle.min.js");
+  }, []);
   return <Component {...pageProps} />
+
 }
