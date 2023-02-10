80ced384a737a75ca5069f8bca1932c76c801198
Lexa <axel.reviron@gmail.com>
2023-02-10 15:26:59 +0100

    import header for all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,17 @@ import type { AppProps } from 'next/app'
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import '@/styles/style.css'
 import { useEffect } from "react";
+import Header from "@/components/Header";
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     require("bootstrap/dist/js/bootstrap.bundle.min.js");
   }, []);
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Header />
+    </>
+  );
 
 }
