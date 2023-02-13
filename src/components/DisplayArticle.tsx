6f8de2f9d4decd6dc91161518219fa52e73d9912
Lexa <axel.reviron@gmail.com>
2023-02-13 17:23:32 +0100

    style for presention of article

--- src/components/DisplayArticle.tsx
+++ src/components/DisplayArticle.tsx
@@ -1,22 +1,30 @@
 
-import {Article} from "@/entities";
+import { Article } from "@/entities";
+import Link from "next/link";
+import { useState } from "react";
 
 interface Props {
-    article:Article;
+    article: Article;
 }
 
-export default function DisplayArticle({article}:Props) {
+export default function DisplayArticle({ article }: Props) {
+
 
     return (
         <>
-            <h1>Titre : {article.title}</h1>
-            <p>{article.text}</p>
-            {article.publication_date &&
-            <p>{new Date(article.publication_date).toLocaleDateString()}</p>}
-            <p>Autheur : {article.author}</p>
-            <p>Vues : {article.view}</p>
-            <img src={article.image} alt="illustration"/>
-            <br/>
+            <div className="col-12 col-md-3 m-5 d-flex flex-column align-items-center">
+                <h1 className="text-center">{article.title}</h1>
+                <Link href={`/article/${article.id}`}>
+                    <img className="mt-3" src={article.image} style={{ width: "100%" }} alt="illustration" />
+                </Link>
+                <div className="d-flex flex-row justify-content-between" style={{ width: "100%" }}>
+                    <p><span className="fw-bold">Auteur : </span>{article.author}</p>
+                    {article.publicationDate &&
+                        <p><span className="fw-bold">Date : </span>{new Date(article.publicationDate).toLocaleDateString()}</p>}
+                    <p><span className="fw-bold">Vues : </span>{article.view}</p>
+                </div>
+                <br />
+            </div>
         </>
     )
 
