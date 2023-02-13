8946671104c7d5d23124e2b1b18f2be7325b9690
Lexa <axel.reviron@gmail.com>
2023-02-13 14:21:57 +0100

    useEffect for API and call to DisplayArticle

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,26 @@
+import { useEffect, useState } from "react";
+import { fetchAllArticles } from "@/service-article";
+import { Article } from "@/entities";
+import DisplayArticle from "@/components/DisplayArticle";
+
 
 export default function Index() {
 
+  const [articles, setArticles] = useState<Article[]>([]);
 
-  return;
+  useEffect(() => {
+    fetchAllArticles().then(data => {
+      setArticles(data);
+    })
+  }, [])
+
+
+  return (
+    <>
+      {articles.map((item) =>
+        <DisplayArticle key={item.id} article={item}/>
+      )}
+    </>
+  )
+
 }
