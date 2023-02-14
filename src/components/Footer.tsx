149901f32a6f836cfc1290c4909fde30f84248d0
Lexa <axel.reviron@gmail.com>
2023-02-14 22:24:10 +0100

    add footer tag

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,14 @@
 export default function Footer() {
     return (
         <>
-            <hr/>
-            <div className="row">
-                <div className="col-12 text-center">
-                    <a href="https://axel-reviron.netlify.app/" className="footerLink">Axel Reviron</a>
+            <footer>
+                <hr />
+                <div className="row">
+                    <div className="col-12 text-center">
+                        <a href="https://axel-reviron.netlify.app/" className="footerLink">Axel Reviron</a>
+                    </div>
                 </div>
-            </div>
+            </footer>
         </>
     );
 }
