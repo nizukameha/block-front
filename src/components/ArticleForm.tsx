7652ef4319d118f91faa8c5d4ef8d6d9d759fd63
Lexa <axel.reviron@gmail.com>
2023-02-14 14:32:38 +0100

    input values to new Object

--- src/components/ArticleForm.tsx
+++ src/components/ArticleForm.tsx
@@ -1,30 +1,66 @@
+import { useState } from "react";
+import { Article } from "@/entities";
+import { AddOneArticle } from "@/service-article";
+import { FormEvent } from "react";
+import dateFormat from "dateformat";
+import { useRouter } from "next/router";
 
 
 export default function ArticleForm() {
 
+    const router = useRouter();
+    const dateNow = new Date()
+    // J'utilise ça pour formater la date au meme format que celui de la base de donnée
+    const pDate = dateFormat(dateNow, "yyyy/mm/dd/")
+    const [article, setArticle] = useState<Article>({
+        author: "",
+        title: "",
+        publicationDate: pDate,
+        image: "",
+        text: "",
+        view : 0
+    });
+
+    function handleChange(event:any) {
+        setArticle({
+            ...article,
+            [event.target.name]: event.target.value
+        });
+    }
+
+
+    async function handleSubmit(event:FormEvent) {
+        event.preventDefault();
+        const added = await AddOneArticle(article)
+
+        router.push('/article/'+added.id);
+
+    }
+
+
     return (
         <>
         <h1 className="text-center mt-3 mb-5">Ajouter un article</h1>
-            <form>
+            <form className="col-6" onSubmit={handleSubmit}>
                 <div className="mb-3">
                     <label htmlFor="formPseudo" className="form-label">Pseudo</label>
-                    <input type="text" className="form-control" id="formPseudo"/>
+                    <input type="text" name="author" className="form-control" id="formPseudo" onChange={handleChange}/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="formTitle" className="form-label">Titre de l'article</label>
-                    <input type="text" className="form-control" id="formTitle"/>
+                    <input type="text" name="title" className="form-control" id="formTitle" onChange={handleChange}/>
                 </div>
                 <div className="mb-3">
                     <label htmlFor="formImg" className="form-label">Lien vers une image</label>
-                    <input type="text" className="form-control" id="formImg"/>
+                    <input type="text" name="image" className="form-control" id="formImg" onChange={handleChange}/>
                 </div>
                 <div className="form-floating mb-3">
-                    <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea2" style={{height:"100px"}}></textarea>
+                    <textarea className="form-control" name="text" placeholder="Leave a comment here" id="floatingTextarea2" style={{height:"100px"}} onChange={handleChange}></textarea>
                     <label htmlFor="floatingTextarea2">Article</label>
                 </div>
                 <label htmlFor="category" className="mb-3">Catégorie</label>
                 <div className="form-check" id="category">
-                    <input className="form-check-input" type="checkbox" value="actualité" id="actualité"/>
+                    <input className="form-check-input" type="checkbox" value="actualité" id="actualité" />
                     <label className="form-check-label" htmlFor="actualité">
                     actualité
                     </label>
