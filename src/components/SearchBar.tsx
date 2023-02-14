b20b0529a152ef48fd18b938e8b6038bab0a290d
Lexa <axel.reviron@gmail.com>
2023-02-14 11:42:53 +0100

    add structure for searchBAr

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,16 @@
+
+
+export default function SearchBar() {
+
+    return (
+        <>
+        <div className="d-flex align-items-center">
+            <label htmlFor="search">
+                <input type="text" name="search" id="search" />
+            </label>
+            <button type="submit" className="ms-2 btn btn-dark">Rechercher</button>
+        </div>
+        </>
+    )
+
+}
