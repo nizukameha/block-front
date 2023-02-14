c159b98a1e1ad4770669b8e14da3687dcb4ff800
Lexa <axel.reviron@gmail.com>
2023-02-14 11:43:19 +0100

    page for add article

---
+++ src/pages/add-article.tsx
@@ -0,0 +1,18 @@
+import ArticleForm from "@/components/ArticleForm";
+
+
+export default function addArticle() {
+
+    return (
+        <>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col-12 d-flex flex-column align-items-center">
+                    <ArticleForm/>
+                </div>
+            </div>
+        </div>
+        </>
+    )
+
+}
